28cca8ee376e665d5e1efee78f53a5293cdc89b0
gursoyasin <gursoyreal@gmail.com>
2026-01-26 18:48:09 +0300

    Initial Commit

---
+++ ıcy/_cyApp.swift
@@ -0,0 +1,17 @@
+//
+//  _cyApp.swift
+//  ıcy
+//
+//  Created by yacN on 26.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct _cyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
